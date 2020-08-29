7094b3733e7d244b16109244c191001c6201b7d6
Josuah Demangeon <me@josuah.net>
2020-08-30 00:29:46 +0200

    getting ready for lwan!

--- lib/cmd_monitor.sh
+++ lib/cmd_monitor.sh
@@ -4,8 +4,8 @@ cmd_monitor_monitor() {
 
 	var_timeout=5
 
-	. "$lib/monitor_$name" >&2 & pid=$!
+	. "$lib/monitor_$name.sh" >&2 & pid=$!
 	sleep "$var_timeout" && kill "$pid" 2>/dev/null &
 	wait "$pid" && status=ok || status=err
-	echo $status do=monitor name=$name $all_vars
+	echo $status do=monitor name=$name $var_export
 }
--- lib/monitor_tls.sh
+++ lib/monitor_tls.sh
@@ -1,4 +1,4 @@
 exec openssl s_client \
   -connect "$var_ip:$var_port" \
-  -verify_return_error -verify_dept 3 \
+  -verify_return_error -verify_depth 3 \
   </dev/null
