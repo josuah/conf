ba948da6ba2be2f277ebdffadeda7e9ba4e1ecae
Josuah Demangeon <me@josuah.net>
2020-08-30 11:29:07 +0200

    symlink qmail's directories at build time

--- build/notqmail/build.sh
+++ build/notqmail/build.sh
@@ -2,12 +2,26 @@ v=1.08
 url=https://github.com/notqmail/notqmail/releases/download/notqmail-$v/notqmail-$v.tar.gz
 sha256=7b0d2153eedaa97988444334a24a0553ee25f58a6bb0521bc6d419bfa6bf7d10
 
+_build_symlink() {
+	mkdir -p "$1"
+	ln -s "$1" "$tmp/$(basename "$2")"
+	mkdir -p "$(dirname "$2")"
+	mv "$tmp/$(basename "$2")" "$(dirname "$2")"
+}
+
 build() {
 	make NROFF=true it man
-	DESTDIR= make setup
 
-	mkdir -p "$DESTDIR$PREFIX"
-	cp -r /var/qmail/bin "$DESTDIR$PREFIX"
-	rm -rf /var/qmail/bin
-	ln -sf "$PREFIX/bin" /var/qmail
+	_build_symlink	/etc/qmail/control	/var/qmail/control
+	_build_symlink	/etc/qmail/alias	/var/qmail/alias
+	_build_symlink	/etc/qmail/users	/var/qmail/users
+	_build_symlink	/var/spool/qmail	/var/qmail/queue
+	_build_symlink	"$PREFIX$DESTDIR/bin"	/var/qmail/bin
+	_build_symlink	"$PREFIX$DESTDIR/man"	/var/qmail/man
+
+	make setup
+
+	rm -f /var/qmail/bin /var/qmail/man
+	_build_symlink	"$PREFIX/bin"		/var/qmail/bin
+	_build_symlink	"$PREFIX/share/man"	/var/qmail/man
 }
--- module/qmail/deploy.sh
+++ module/qmail/deploy.sh
@@ -1,20 +1,9 @@
 #!/bin/sh -eux
 
-link() {
-	ln -s "$1" "$tmp/$(basename "$2")"
-	mkdir -p "$(dirname "$2")"
-	mv "$tmp/$(basename "$2")" "$(dirname "$2")"
-}
-
 tmp=$(mktemp -d)
 trap 'rm -rf "$tmp"' INT TERM EXIT HUP
 
 mkdir -p "/etc/qmail/control"
 hostname >/etc/qmail/control/me
 
-mkdir -p /var/qmail
-link	/etc/qmail/control	/var/qmail/control
-link	/etc/qmail/alias	/var/qmail/alias
-link	/etc/qmail/users	/var/qmail/users
-link	/var/spool/qmail	/var/qmail/queue
-link	/usr/adm/bin		/var/qmail/bin
+cd /etc/usr/
