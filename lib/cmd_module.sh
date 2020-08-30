c3393049e33da39b41f1580da849585268be5433
Josuah Demangeon <me@josuah.net>
2020-08-30 20:28:21 +0200

    die on misformatted template

--- lib/cmd_module.sh
+++ lib/cmd_module.sh
@@ -1,24 +1,27 @@
 
+_module_template() (
+	cd "$etc/data"
+	adm-template "$etc/module/$name/$1" >$tmp/module/${1%.template}
+)
+
 cmd_module_install() {
 	local name="$1"
-	local path_template="$etc/module/$name/template"
 
 	if [ -d "$etc/module/$name/etc" ]; then
-		(cd "$etc/module/$name"; find etc -type f) | while read x; do
+		while read x; do
 			mkdir -p "$(dirname "$tmp/module/$x")"
 
 			case $x in
 			(*.template)
-				(cd "$etc/data"; adm-template) \
-				  <$etc/module/$name/$x \
-				  >$tmp/module/${x%.template}
+				_module_template "$x" || exit 1
 				;;
 			(*)
 				cp "$etc/module/$name/$x" "$tmp/module/$x"
 				;;
 			esac
-		done
-
+		done <<EOF
+$(cd "$etc/module/$name"; find etc -type f)
+EOF
 		cp -r "$tmp/module/etc" "/"
 	fi
 
