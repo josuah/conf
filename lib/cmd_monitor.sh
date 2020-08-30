a13a84eeecc3630f7c0a8b84e079f76b268e5c32
Josuah Demangeon <me@josuah.net>
2020-08-30 01:47:48 +0000

    fix parameters passed to cmd_monitor_monitor()

--- lib/cmd_monitor.sh
+++ lib/cmd_monitor.sh
@@ -1,11 +1,12 @@
 
 cmd_monitor_monitor() {
 	local name="$1"
+	shift 1
 
 	var_timeout=5
 
 	. "$lib/monitor_$name.sh" >&2 & pid=$!
 	sleep "$var_timeout" && kill "$pid" 2>/dev/null &
 	wait "$pid" && status=ok || status=err
-	echo $status do=monitor name=$name $var_export
+	echo $status do=monitor name=$name "$@"
 }
