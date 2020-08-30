927523075e9b2a814910d57a9ac34dee0fa5c6ae
Josuah Demangeon <me@josuah.net>
2020-08-30 08:29:53 +0000

    fix NetBSD's path

--- lib/cmd_golang.sh
+++ lib/cmd_golang.sh
@@ -1,4 +1,5 @@
 
 cmd_golang_install() {
+	[ -f "${var_cmd:-$1}" ] && return 0
 	GOPATH=/usr/go go get "$1"
 }
--- module/netbsd/etc/profile.d/netbsd.sh
+++ module/netbsd/etc/profile.d/netbsd.sh
@@ -1,3 +1,4 @@
 export PKG_PATH="http://cdn.NetBSD.org/pub/pkgsrc/packages"
 export PKG_PATH="$PKG_PATH/$(uname -s)/$(uname -m)"
 export PKG_PATH="$PKG_PATH/$(uname -r | cut -d . -f 1-2)/All/"
+export PATH=$PATH:/usr/pkg/libexec/git-core
