49e81bf6e42df46f7ffd201189a661a81028e671
Josuah Demangeon <me@josuah.net>
2020-09-12 20:09:23 +0200

    switch from lib/adm/monitor_* to check-*

--- lib/adm/cmd_monitor.sh
+++ lib/adm/cmd_monitor.sh
@@ -5,7 +5,7 @@ cmd_monitor_monitor() {
 
 	var_timeout=5
 
-	. "$lib/monitor_$name.sh" >&2 & pid=$!
+	"check-$name" >&2 & pid=$!
 	sleep "$var_timeout" && kill "$pid" 2>/dev/null &
 	wait "$pid" && status=ok || status=err
 	echo $status do=monitor name=$name "$@"
