5975f2a8c8cd9a8868a7ad85456b9cdd17b3322e
Josuah Demangeon <me@josuah.net>
2020-09-13 23:23:53 +0200

    integrate quotes in adm too

---
+++ lib/adm/cmd_exec.sh
@@ -0,0 +1,30 @@
+
+cmd_exec_start() { set -eu
+	local cmd="$1"
+	local argv="$cmd${var_argv:+ $var_argv}"
+	local pexp=${var_pexp:-$argv}
+
+	pgrep -f "$pexp" && return
+	$argv 2>&1 | logger -t "$cmd" &
+}
+
+cmd_exec_stop() { set -eu
+	local cmd="$1"
+	local argv="$cmd${var_argv:+ $var_argv}"
+	local pexp=${var_pexp:-$argv}
+
+	pkill -f "$pexp"
+}
+
+cmd_exec_status() { set -eu
+	local cmd="$1"
+	local argv="$cmd${var_argv:+ $var_argv}"
+	local pexp=${var_pexp:-$argv}
+
+	pgrep -f "$pexp"
+}
+
+cmd_exec_restart() { set -eu
+	cmd_exec_stop "$1"
+	cmd_exec_start "$1"
+}
