37d70e00e709953ac298a5dbed3ea9f66f4aa209
Josuah Demangeon <me@josuah.net>
2020-09-13 11:44:31 +0200

    notqmail

--- module/qmail/deploy.pre.sh
+++ module/qmail/deploy.pre.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -eux
 
-mkdir -p "/etc/qmail/control"
+mkdir -p /etc/qmail /var/spool
+ln -fs /var/qmail/alias /var/qmail/control /var/qmail/users /etc/qmail
+ln -fs /var/qmail/queue /etc/qmail/qmail
+mv -f /etc/qmail/qmail /var/spool
+
 hostname >/etc/qmail/control/me
