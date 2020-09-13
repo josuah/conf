942f895ae43eb3b91ecf4ac8cab320d61729b1fb
Josuah Demangeon <me@josuah.net>
2020-09-13 19:54:34 +0200

    fix lwan install script

--- build/lwan/build.sh
+++ build/lwan/build.sh
@@ -2,8 +2,8 @@ url=https://github.com/lpereira/lwan
 commit=a07f949141f38e46c09561b30dfb0cb31b70be35
 
 build() { set -eux
-	mkdir build
+	mkdir -p build
 	cd build
-	cmake ..
-	make
+	cmake -D"CMAKE_INSTALL_PREFIX"="$PREFIX" ..
+	make PREFIX="$PREFIX" DESTDIR="$DESTDIR" install
 }
