e9dae2dd4dda67d081a91b790fc7bb37d6a1aa23
Josuah Demangeon <me@josuah.net>
2020-09-13 16:29:35 +0200

    fix qmail module deployment

--- module/qmail/deploy.pre.sh
+++ module/qmail/deploy.pre.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -eux
 
-mkdir -p /etc/qmail /var/spool
-ln -fs /var/qmail/alias /var/qmail/control /var/qmail/users /etc/qmail
+mkdir -p /var/qmail/alias /var/qmail/control /var/qmail/users /etc/qmail \
+	/var/spool
+
+ln -fs /var/qmail/alias /var/qmail/control /var/qmail/users \
+	/etc/qmail
+
 ln -fs /var/qmail/queue /etc/qmail/qmail
 mv -f /etc/qmail/qmail /var/spool
 
