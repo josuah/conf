4ed844e416a5de9257d365c307854d2e2590450d
Josuah Demangeon <me@josuah.net>
2020-09-17 03:37:44 +0200

    deduplicate loop

--- module/monitower/lib.sh
+++ module/monitower/lib.sh
@@ -1,15 +1,27 @@
 
+_monitower_dns() {
+	adm-db "$etc/db/dns/alias" get dom soa | while read dom soa; do
+		dom=${dom#@}
+		dom=${dom:+$dom.}$soa
+		echo "$* type=a dom=$dom"
+	done
+}
+
 deploy_pre() { set -eu
-	local out="$dest/etc/monitor/host"
+	local line
 
-	mkdir -p "$dest/etc/monitower/queue.table"
+	mkdir -p "$dest/etc/monitower"
 
-	for host in $(adm-db "$etc/db/host/ip" get pub=true host | sort -u); do
-		for ip in $(adm-db "$etc/db/host/ip" get ip pub=true host="$host"); do
-			adm list node/$host | while IFS=' /' read type name vars; do
-				[ "$type" = "monitor" ] || continue
-				echo "host=$host ip=$ip check=$name $vars"
-			done
+	adm-db "$etc/db/host/ip" get pub=true host ip | while read host ip; do
+		adm list "host/$host" | while IFS=' /' read type name vars; do
+
+			line="host=$host ip=$ip check=$name $vars"
+
+			case $type/$name in
+			(monitor/dns) _monitower_dns $line ;;
+			(monitor/*) echo "$line" ;;
+			(*) continue ;;
+			esac
 		done
 	done >$dest/etc/monitower/queue
 }
