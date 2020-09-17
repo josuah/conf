871abc7383692ed462e956f04ad84ce354a569b3
Josuah Demangeon <me@josuah.net>
2020-09-17 02:12:37 +0200

    simplify further, remove unused modules, prepare monitoring

--- lib/adm/cmd_monitor.sh
+++
@@ -1,9 +0,0 @@
-
-cmd_monitor_monitor() { set -eu
-	local name="$1" ip
-	shift
-
-	for ip in $(adm-db "$etc/db/host/ip" get ip host="$host" pub=true); do
-		echo "name=$name" ip="$ip" "$@"
-	done
-}
--- module/monitor/lib.sh
+++ module/monitor/lib.sh
@@ -3,7 +3,7 @@ deploy_pre() { set -eu
 	local out="$dest/etc/monitor/host"
 
 	rm -rf "/etc/module/host"
-	mkdir -p "$dest/etc/monitor/host"
+	mkdir -p "$dest/etc/host"
 
 	for host in $(adm-db "$etc/db/host/ip" get pub=true host | sort -u); do
 		(cd "$etc/db" && adm-db "$conf/host.conf" template >$out/$host.conf)
