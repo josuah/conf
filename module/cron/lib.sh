b612d3d6ee8c0c2d240ff673135ce5e4a2c6bb62
Josuah Demangeon <me@josuah.net>
2020-09-17 04:50:27 +0200

    enable monitower's cron

--- module/cron/lib.sh
+++ module/cron/lib.sh
@@ -1,14 +1,6 @@
 
 deploy_post() { set -eu
-{
-	echo "#Min	Hour	DoM	Mon	DoW	User	Command"
-	for user in $(ls /etc/crontab.d/); do
-		cat "/etc/crontab.d/$user/"* | while read t1 t2 t3 t4 t5 cmd; do
-			printf '%s\t' "$t1" "$t2" "$t3" "$t4" "$t5"
-			printf '%s\t%s\n' "$user" "$cmd"
-		done
-	done
-}  >/etc/crontab
-
+	echo "#Min	Hour	DoM	Mon	DoW	User	Command" \
+	| cat - /etc/crontab.d/* >/etc/crontab
 	pkill -HUP cron
 }
