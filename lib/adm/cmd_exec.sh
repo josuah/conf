205e03a5fbe2cd1313cf5d5b0977cb1610d5c5a4
Josuah Demangeon <me@josuah.net>
2020-09-21 00:19:08 +0200

    add support for systemd's "socket activation" thing through s6

--- lib/adm/cmd_exec.sh
+++ lib/adm/cmd_exec.sh
@@ -1,11 +1,29 @@
 
+# draws a dependency onto s6-networking onto the core
+
+_exec_systemd_socket() ( set -eu
+	local v="$1" ip="$2" port="$3" argv="$4"
+	export LISTEN_FDS=1
+
+)
+
 cmd_exec_start() { set -eu
 	local cmd="$1"
 	local argv="$cmd${var_argv:+ $var_argv}"
 	local pexp="${var_pexp:-$argv}"
 
 	pgrep -f "$pexp" >/dev/null && return
-	$argv 2>&1 | logger -t "$cmd" &
+
+	if [ "${var_systemd_socket:-}" ]; then
+		export LISTEN_FDS=1
+		s6-tcpserver4-socketbinder 0.0.0.0 "$var_port" sh -c "exec $argv 3>&0" \
+		| logger -t "$cmd" &
+		s6-tcpserver6-socketbinder :: "$var_port" sh -c "exec $argv 3>&0" \
+		| logger -t "$cmd" &
+	else
+		sh -c "exec $argv" | logger -t "$cmd"
+	fi
+
 	cmd_exec_status "$cmd"
 }
 
