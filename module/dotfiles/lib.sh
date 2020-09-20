ea2546f84837e94abad1a2a4d12e7382cab241e4
Josuah Demangeon <me@josuah.net>
2020-09-21 00:20:42 +0200

    copy dotfiles rather than symlinking

--- module/dotfiles/lib.sh
+++ module/dotfiles/lib.sh
@@ -1,10 +1,6 @@
 
 deploy_pre() { set -eu
-
-	find * -name "lib.sh" -o \
-		-type d -exec mkdir -p "$HOME/.{}" \; -o \
-		-type f -exec cp "$PWD/{}" "$HOME/.{}" \;
-
+	cp -rjfhome/.* "$HOME"
 	mkdir -p "$HOME/.ssh/sock"
 	touch "$HOME/.ssh/config_local"
 }
