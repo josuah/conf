1f53e17770672cad708d007960959ee208be1a94
Josuah Demangeon <me@josuah.net>
2020-09-26 08:53:18 +0200

    each module has its db

---
+++ module/dovecot/lib.sh
@@ -0,0 +1,23 @@
+
+deploy_pre() { set -eu
+
+	mkdir -p /var/qmail/alias /var/qmail/control /var/qmail/users \
+		/etc/qmail /var/spool
+
+	ln -fs /var/qmail/alias /var/qmail/control /var/qmail/users \
+		/etc/qmail
+
+	ln -fs /var/qmail/queue /etc/qmail/qmail
+	mv -f /etc/qmail/qmail /var/spool
+
+	hostname >/etc/qmail/control/me
+}
+
+deploy_post() { set -eu
+	(cd /etc/qmail/users && qmail-newu)
+	pkill -HUP qmail-start
+	pkill -HUP inetd
+}
+
+export uid="$(id -u mail)"
+export gid="$(id -g mail)"
