4be4e968579fb64a0ede1fd4bb29dc018529b2b3
Josuah Demangeon <me@josuah.net>
2020-09-27 19:26:02 +0200

    fix tls cert paths

--- mod/collect/lib.sh
+++
@@ -1,4 +0,0 @@
-deploy_pre() { set -eu
-	send "ls /etc/ssl/*.crt" | sed -r 's,.*/(.*).crt,dom=\1,' \
-	  >$etc/host/$host/db.cert
-}
--- mod/httpd/lib.sh
+++ mod/httpd/lib.sh
@@ -2,7 +2,7 @@ deploy_pre() { set -eu
 	local acme=/etc/acme/letsencrypt-privkey.pem
 
 	send "
-		mkdir -p -m 700 /etc/acme
+		mkdir -p -m 700 /etc/acme /etc/ssl/private
 		test -f '$acme' || openssl genrsa -out '$acme'
 	"
 }
