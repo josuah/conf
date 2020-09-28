7462a11ffbf89ab3d267df8254ceb9821d51c13b
Josuah Demangeon <me@josuah.net>
2020-09-28 02:09:08 +0200

    prepare to merge dns-axfr and dns-auth

--- lib/cmd_mod.sh
+++ lib/cmd_mod.sh
@@ -1,4 +1,3 @@
-
 cmd_mod_install() { set -eu
 	local name="$1"
 	export conf="$etc/mod/$name"
@@ -6,20 +5,13 @@ cmd_mod_install() { set -eu
 	local trap="rm -rf '$etc/mod/'*'/tmp'"
 
 	trap "$trap" INT TERM EXIT HUP
+	mkdir -p "$dest/etc" "$dest/var"
 
 	. "$etc/mod/$name/lib.sh"
 
-	mkdir -p "$dest/etc" "$dest/var"
-
 	(type deploy_pre >/dev/null && cd "$conf" && deploy_pre)
 
-	if [ -d "$conf/bin" ]; then
-		mkdir -p "$dest/usr/local"
-		cp -rf "$conf/bin" "$dest/usr/local"
-	fi
-
 	for x in $(cd "$conf" && find etc var -type f 2>/dev/null); do
-		[ -f "$conf/$x" ] || continue
 		mkdir -p "$(dirname "$dest/$x")"
 		(cd "$etc" && adm-db "$conf/$x" template >$dest/$x)
 	done
--- mod/dns-auth/lib.sh
+++ mod/dns-auth/lib.sh
@@ -3,9 +3,10 @@ export tsig="$(adm-db "$db/dns-var" get pass | tr -d '\n' | openssl base64 -e)"
 deploy_pre() { set -eu
 	mkdir -p "$dest/var/nsd/zones/master"
 
-	for soa in $(adm-db "$db/dns-zones" get soa); do
-		(cd "$etc" && zone=$soa adm-db "$conf/domain.zone" template) \
+	for soa in $(adm-db "$db/dns-soa" get soa); do
+		(cd "$etc" && zone=$soa adm-db "$conf/zone" template) \
 			>$dest/var/nsd/zones/master/$soa
+		less "$dest/var/nsd/zones/master/$soa"
 	done
 }
 
