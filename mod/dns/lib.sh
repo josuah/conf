9083159928eba7b5ad12baac2615d924d156c414
Josuah Demangeon <me@josuah.net>
2020-09-28 08:59:17 +0200

    adm-db: implement a file cache to allow concurrent reads

--- mod/dns/lib.sh
+++ mod/dns/lib.sh
@@ -2,14 +2,17 @@ export tsig="$(adm-db "$db/dns-var" get pass | tr -d '\n' | openssl base64 -e)"
 export other="$(adm-db "$db/dns-ns" get mode host="$host" | tr xfr_uth uth_xfr)"
 
 deploy_pre() { set -eu
-	mkdir -p "$dest/var/nsd/zones/master"
+	mkdir -p "$dest/var/nsd/auth" "$dest/var/nsd/axfr"
 
 	for soa in $(adm-db "$db/dns-soa" get soa); do
 		(cd "$etc" && zone=$soa adm-db "$conf/zone" template) \
-			>$dest/var/nsd/zones/master/$soa
+			>$dest/var/nsd/auth/$soa
 	done
 }
 
 deploy_post() { set -eu
-	send "pkill -HUP nsd"
+	send "
+		chown _nsd: /var/nsd/auth /var/nsd/axfr
+		pkill -HUP nsd
+	"
 }
