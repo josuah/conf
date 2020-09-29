142712448532139e8ef5aa0ca31c68929e2d73d3
Josuah Demangeon <me@josuah.net>
2020-09-29 23:00:13 +0200

    adjust zone directory

--- mod/dns/lib.sh
+++ mod/dns/lib.sh
@@ -2,17 +2,17 @@ export tsig="$(adm-db "$db/dns-var" get pass | tr -d '\n' | openssl base64 -e)"
 export other="$(adm-db "$db/dns-ns" get mode host="$host" | tr xfr_uth uth_xfr)"
 
 deploy_pre() { set -eu
-	mkdir -p "$dest/var/nsd/auth" "$dest/var/nsd/axfr"
+	mkdir -p "$dest/var/nsd/zones/auth" "$dest/var/nsd/zones/axfr"
 
 	for soa in $(adm-db "$db/dns-soa" get soa); do
 		(cd "$etc" && zone=$soa adm-db "$conf/zone" template) \
-			>$dest/var/nsd/auth/$soa
+			>$dest/var/nsd/zones/auth/$soa
 	done
 }
 
 deploy_post() { set -eu
 	send "
-		chown _nsd: /var/nsd/auth /var/nsd/axfr
+		chown -R _nsd: /var/nsd/*
 		pkill -HUP nsd
 	"
 }
