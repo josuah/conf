34a78b2c97d80ea98e7f61d2ca03c7481ab1c1be
Josuah Demangeon <me@josuah.net>
2020-10-05 00:21:15 +0200

    Autogenerate DNS for all services

--- mod/http/lib.sh
+++ mod/http/lib.sh
@@ -1,8 +1,12 @@
-deploy_pre() { set -eu
+deploy_post() { set -eu
 	local acme=/etc/acme/letsencrypt-privkey.pem
 
 	send "
 		mkdir -p -m 700 /etc/acme /etc/ssl/private
 		test -f '$acme' || openssl genrsa -out '$acme'
+
+		printf 'include \"%s\"\n' /etc/httpd.d/* >>/etc/httpd.conf
+
+		printf 'include \"%s\"\n' /etc/relayd.d/* >/etc/relayd.conf
 	"
 }
