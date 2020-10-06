eb5bf4854c6a4c6d4da70bbb7a97d7d747d4e4c3
Charlie Root <root@olor.z0.is>
2020-10-06 07:35:14 +0000

    use deployment directories

--- mod/mail/lib.sh
+++ mod/mail/lib.sh
@@ -1,19 +1,3 @@
-export uid="$(id -u mail)"
-export gid="$(id -g mail)"
-
-deploy_pre() { set -eu
-	mkdir -p /var/qmail/alias /var/qmail/control /var/qmail/users \
-		/etc/qmail /var/spool
-
-	ln -fs /var/qmail/alias /var/qmail/control /var/qmail/users \
-		/etc/qmail
-
-	ln -fs /var/qmail/queue /etc/qmail/qmail
-	mv -f /etc/qmail/qmail /var/spool
-
-	hostname >/etc/qmail/control/me
-}
-
 deploy_post() { set -eu
 	send "
 		pkill -HUP dovecot
