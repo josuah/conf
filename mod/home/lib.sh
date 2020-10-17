22a1ed80f541e63e48068a9585bf3b04a6ef761a
Josuah Demangeon <me@josuah.net>
2020-10-17 20:22:47 +0200

    is it home? add keys

--- mod/home/lib.sh
+++ mod/home/lib.sh
@@ -1,9 +1,14 @@
 deploy_pre() { set -eu
-	[ "${user:-root}" = root ] && home="root" || home="home/$user"
+	[ "${user:-root}" = root ] && home="/root" || home="/home/$user"
 
-	mkdir -p "$dest/$home"
-	cp -rf dot/.??* "$dest/$home"
+	mkdir -p "$dest$home"
+	cp -rf dot/.??* "$dest$home"
 
-	mkdir -p "$dest/$home/.ssh/sock"
-	cat keys/* >$dest/$home/.ssh/authorized_keys
+	mkdir -p "$dest$home/.ssh/sock"
+	cat keys/* >$dest$home/.ssh/authorized_keys
+
+	send "
+		[ -f '$home/.ssh/id_ed25519' ] ||
+		  ssh-keygen -t ed25519 -N '' -f '$home/.ssh/id_ed25519'
+	"
 }
