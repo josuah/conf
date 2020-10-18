327b1b0a3c33cffe429470dedbfac667e46cf2b7
Charlie Root <root@pelios.z0.is>
2020-10-19 01:02:06 +0200

    dbase: permit self exclusion through key!value operator

--- mod/vpn/lib.sh
+++ mod/vpn/lib.sh
@@ -1,8 +1,5 @@
 deploy_post() { set -eu
 	send "
-		cd /etc/wireguard
-		umask 400
-		[ -f key ] || echo PrivateKey = \$(wg genkey) >key
-		cat interface key peers >conf
+		chmod 700 /etc/wireguard
 	"
 }
