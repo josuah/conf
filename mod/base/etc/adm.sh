e07b3e0e7045c85e436070e63f0540df726c6f73
Josuah Demangeon <me@josuah.net>
2020-10-24 12:06:24 +0200

    preparing a CA with bin/x509

--- mod/base/etc/adm.sh
+++ mod/base/etc/adm.sh
@@ -6,14 +6,14 @@ printf '\n  %s\n\n' "$(uname -srnm)"
 
 exec >/etc/crontab
 cat /etc/crontab.d/*
-pkill -HUP cron
+! pkill -HUP cron
 
 exec >/etc/syslog.conf
 cat /etc/syslog.d/*
 sed -n 's,^[^#/]*/,/,p' /etc/syslog.conf | xargs touch
 sed -n 's,^[^#/]*/,/,p' /etc/syslog.conf | xargs chmod 6400
 sed -n 's,^[^#/]*/,/,p' /etc/syslog.conf | xargs chown root:
-pkill -HUP syslogd
+! pkill -HUP syslogd
 
 exec >/etc/newsyslog.conf
 cat /etc/newsyslog.d/*
--- mod/base/lib.sh
+++ mod/base/lib.sh
@@ -2,11 +2,11 @@ deploy_post() { set -eu
 	send "
 		printf '\n  %s\n\n' \"\$(uname -srnm)\" >/etc/motd
 
+		sh /etc/adm.sh
+
 		mkdir -p /var/unbound/db
 		chown _unbound: /var/unbound/db
 		unbound-anchor -vvv -a /var/unbound/db/root.key
-
-		sh /etc/adm.sh
 
 		mkdir -p '$usr'
 	"
