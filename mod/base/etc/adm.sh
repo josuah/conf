7616122d1b03f117ec839547f3a2c68eed80d262
Josuah Demangeon <me@josuah.net>
2020-11-02 23:04:42 +0100

    add dhcp on bridge0

--- mod/base/etc/adm.sh
+++ mod/base/etc/adm.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
-set -eu
 
 exec >/etc/motd
 printf '\n  %s\n\n' "$(uname -srnm)"
 
 exec >/etc/crontab
 cat /etc/crontab.d/*
-! pkill -HUP cron
+pkill -HUP cron
 
 exec >/etc/syslog.conf
 cat /etc/syslog.d/*
 sed -n 's,^[^#/]*/,/,p' /etc/syslog.conf | xargs touch
 sed -n 's,^[^#/]*/,/,p' /etc/syslog.conf | xargs chmod 6400
 sed -n 's,^[^#/]*/,/,p' /etc/syslog.conf | xargs chown root:
-! pkill -HUP syslogd
+pkill -HUP syslogd
 
 exec >/etc/newsyslog.conf
 cat /etc/newsyslog.d/*
@@ -29,3 +28,8 @@ cat /etc/hosts.d/*
 
 exec >/etc/pf.conf
 cat /etc/pf.d/*
+
+exec >/etc/dhcpd.conf
+cat /etc/dhcpd.d/*
+
+true
