32ca21aa76e89ff5ebf399d59e28644b69d1edcf
Josuah Demangeon <me@josuah.net>
2021-04-06 03:58:57 +0200

    simplify rrzone a bit more

--- dns/sign.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -eu
-
-export KSK="$(ldns-keygen -k -a RSASHA512 -b 4096 "$zone")"
-export ZSK="$(ldns-keygen -a RSASHA512 -b 4096 "$zone")"
-
-ldns-sigzone -n -s "$(openssl rand -base64 24)"
