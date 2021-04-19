cf39fc549c0c8611c67bcbc2ecb050c049016e99
Josuah Demangeon <me@josuah.net>
2021-04-19 04:18:43 +0200

    update openbgpd on linux

--- recipe/openbgpd.sh
+++ recipe/openbgpd.sh
@@ -1,2 +1,26 @@
-ver=6.8p1
-url=https://cdn.openbsd.org/pub/OpenBSD/OpenBGPD/openbgpd-$ver.tar.gz
+ver=master
+url=https://github.com/openbgpd-portable/openbgpd-portable.git
+
+pack_download() {
+	pack_download_git
+	cd "$SOURCE"
+
+patch -p1 <<'__EOF__'
+--- old/src/bgpd/kroute-disabled.c
++++ new/src/bgpd/kroute-disabled.c
+@@ -305,6 +305,9 @@
+ 		struct sockaddr_in6 *m6;
+ 		int plen;
+ 
++		if (!ifa->ifa_addr)
++			continue;
++
+ 		switch (ifa->ifa_addr->sa_family) {
+ 		case AF_INET:
+ 			m4 = (struct sockaddr_in *)ifa->ifa_netmask;
+__EOF__
+
+}
+pack_build() {
+	make
+}
