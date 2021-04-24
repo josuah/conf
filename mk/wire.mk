7b23b6fb711a20fde45159397f86bd067c7ec951
Josuah Demangeon <me@josuah.net>
2021-04-24 16:09:28 +0200

    wire: convert to awk update

---
+++ mk/wire.mk
@@ -0,0 +1,11 @@
+CONF = nsd/nsd.conf
+include mk/conf.inc
+
+mk/dns: ${CONF}
+	exec rm -rf /var/nsd/zones
+	exec mkdir -p /var/nsd
+	exec cp -r zones /var/nsd
+	exec nsd-control reload
+
+mk/dns/sync:
+mk/dns/clean:
