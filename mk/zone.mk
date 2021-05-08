7072f783c1d2e0ce7c2e4a8dbea4611bdaa28215
Josuah Demangeon <me@josuah.net>
2021-05-08 21:57:55 +0200

    zone: make the data and sync steps separate

--- mk/zone.mk
+++ mk/zone.mk
@@ -1,13 +1,13 @@
 ZONE = z0.is z0.dn42 josuah.net metairies.org
 NS = ns1.z0.is ns2.z0.is
 
-mk/zone:
+mk/zone: sign
 mk/zone/sync: ${NS}
 
 mk/zone/clean:
 	exec rm -f zone/*
 
-${NS}: sign
+${NS}:
 	template conf/nsd/nsd.conf | ssh $@ 'exec cat >/var/nsd/etc/nsd.conf'
 	exec rsync -rt --delete zone/ $@:/var/nsd/zones/
 	exec ssh $@ exec nsd-control reload
@@ -15,13 +15,13 @@ ${NS}: sign
 sign zsk ksk: zone
 	for zone in zone/*.*.zone; do doas dnssec $@ "$$zone"; done
 
-zone: zone/sshfp
+zone: zone/sshfp.zone
 	exec mkdir -p zone
 	(cd conf/zone && template ${ZONE:=.zone}) | (cd zone && zone)
 	exec cat zone/sshfp >>zone/z0.is.zone
 	exec cat zone/sshfp >>zone/z0.dn42.zone
 
-zone/sshfp:
+zone/sshfp.zone:
 	dnssec sshfp conf/zone/z0.is.zone | sort -o $@
 
 .PHONY: zone
