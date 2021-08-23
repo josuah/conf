193799a8625ad159e1aaa6e871fa4fb26b8fdd61
Josuah Demangeon <me@josuah.net>
2021-08-23 08:48:27 +0200

    wireguard: use fe80:: and eventually route more

--- mk/sync.mk
+++ mk/sync.mk
@@ -1,7 +1,8 @@
 SYNC_CONF = lil1 ams1 tyo1 term1 # dis-sys-2
-SYNC_HOME = root@term1 backup@term1 josuah@term1 root@lil1 josuah@lil1 \
- backup@lil1 git@lil1 josuah@bitreich.org josuah@server10.openbsd.amsterdam \
- root@ams1 josuah@ams1
+SYNC_HOME = root@term1 backup@term1 josuah@term1 \
+ root@lil1 backup@lil1 git@lil1 \
+ root@ams1 josuah@ams1 \
+ josuah@bitreich.org josuah@server10.openbsd.amsterdam \
 # root@dis-sys-2 jdemangeon@dis-sys-2
 
 sync: ${SYNC_CONF} ${SYNC_HOME}
