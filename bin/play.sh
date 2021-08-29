82d3ff4727a5fc4254d8bea6b159d33d12c68e39
Josuah Demangeon <me@josuah.net>
2021-08-29 20:41:20 +0200

    play: case-insensitive search

--- bin/play.sh
+++ bin/play.sh
@@ -2,4 +2,4 @@
 # play a piece of media from local collection
 
 cd /mnt/media
-find * | sort | menu -l 20 | tr '\n' '\0' | xargs -0r mpv
+find * | sort | menu -i -l 20 | tr '\n' '\0' | xargs -0r mpv
