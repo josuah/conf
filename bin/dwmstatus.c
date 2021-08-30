093d485e88ae472830bf0fa932dbf5bfcc2a3155
Josuah Demangeon <me@josuah.net>
2021-08-30 08:45:00 +0200

    dwmstatus: nimble status for dwm

---
+++ bin/dwmstatus.c
@@ -0,0 +1,34 @@
+#include <X11/Xlib.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include "util.h"
+
+int
+main(void)
+{
+	Display *dpy;
+	char line[128] = {0};
+
+	if ((dpy = XOpenDisplay(NULL)) == NULL)
+		err(1, "cannot open display: %s", strerror(errno));
+
+	while (1) {
+		char datetime[32] = {0};
+		time_t clock;
+		double loadavg[3];
+
+		clock = time(NULL);
+		strftime(datetime, sizeof datetime, "%Y-%m-%d %H:%M:%S", localtime(&clock));
+
+		if (getloadavg(loadavg, 1) == -1)
+			warn("getloadavg: %s", strerror(errno));
+
+		snprintf(line, sizeof line, " %.02f %s ", loadavg[0], datetime);
+
+		XStoreName(dpy, DefaultRootWindow(dpy), line);
+		XSync(dpy, False);
+
+		sleep(1);
+	}
+}
