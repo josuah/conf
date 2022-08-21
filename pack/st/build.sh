561ab195ca717f3ffa487167135410b89c29e392
Josuah Demangeon <me@josuah.net>
2022-08-21 23:26:02 +0200

    show last error code in PS1

--- pack/st/build.sh
+++ pack/st/build.sh
@@ -3,5 +3,5 @@ export V=master
 pack_download_git git://git.suckless.org/st
 sed -ri 's/^#(FREETYPEINC *=)/\1/' config.mk
 sed -ri 's/ -lrt / /' config.mk
-cp /etc/pack/st.config.h config.h
+cp /etc/pack/st/config.h config.h
 pack_make
