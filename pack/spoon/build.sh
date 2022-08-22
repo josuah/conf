3d6ca7b6de020ece498827e34c9cdf2ac9662086
Josuah Demangeon <me@josuah.net>
2022-08-22 10:40:44 +0200

    merge changes from ams1

--- pack/spoon/build.sh
+++ pack/spoon/build.sh
@@ -3,4 +3,5 @@ export V=master
 pack_download_git git://git.2f30.org/spoon
 pack_configure
 sed -i 's/.*+=.*mpd/#/' Makefile
-cp /etc/pack/spoon.config.h config.h
+cp /etc/pack/spoon/config.h config.h
+pack_make
